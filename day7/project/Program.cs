0eb74547c231c88c27caa6df9b5619d1e7c32e6a
Javohir <javohirsadullayev2024@gmail.com>
2026-01-27 16:36:13 +0500

    day7

---
+++ day7/project/Program.cs
@@ -0,0 +1,57 @@
+﻿// class ManagedOnly
+// {
+//   public string Name {get;set;} = "Shalola";
+// }
+
+// class Program
+// {
+//   static void Main()
+//   {
+//     ManagedOnly obj = new ManagedOnly();
+//     Console.WriteLine(obj.Name);
+//   }
+// }
+
+using System;
+using System.IO;
+
+// class Program
+// {
+//   static void Main()
+//   {
+//     var fs = new FileStream("a.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+//     fs.Dispose();
+//   }
+// }
+
+// class Program
+// {
+//   static void Main()
+//   {
+//     using (var fs = new FileStream("a.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+//     using (var writer = new StreamWriter(fs))
+//     {
+//       writer.WriteLine("Watch");
+//     }
+//   }
+// }
+
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+  static void Main()
+  {
+    using (var client = new TcpClient("example.com", 80))
+    using (NetworkStream stream = client.GetStream())
+    {
+      string req = "Get / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
+      byte[] data = Encoding.ASCII.GetBytes(req);
+
+      stream.Write(data, 0, data.Length);
+    }
+  }
+}
