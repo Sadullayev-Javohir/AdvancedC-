9e8bca10df02127c0d02520e5d27efb83acc70f0
Javohir <javohirsadullayev2024@gmail.com>
2026-01-29 12:22:56 +0500

    day8

---
+++ day8/project/Program.cs
@@ -0,0 +1,46 @@
+﻿// using System.Reflection;
+
+// Type t = typeof(User);
+
+// var methods = t.GetMethods();
+
+// // foreach (var method in methods)
+// // {
+// //     Console.WriteLine(method.Name);
+// // }
+
+// MethodInfo mi = t.GetMethod("Hello");
+// mi.Invoke(obj, null);
+
+// // Type t = typeof(User);
+// // object obj = Activator.CreateInstance(t);
+
+// Console.WriteLine(obj.GetType().Name);
+// class User
+// {
+//   public void Hello()
+//   {
+//       Console.WriteLine("Hello, User!");
+//   }
+
+//   public void Goodbye()
+//   {
+//       Console.WriteLine("Goodbye, User!");
+//   }
+// }
+
+
+
+using System.Reflection;
+
+Type t = typeof(User);
+
+Console.WriteLine(t.Name);
+Console.WriteLine(t.Assembly.FullName);
+Console.ReadLine();
+class User
+{
+    public string Name { get; set; }
+    public void SayHello() => Console.WriteLine("Hello");
+}
+
