44d146b0fcebe559c7d24f027d1c49547ee144bc
Javohir <javohirsadullayev2024@gmail.com>
2026-02-12 21:57:49 +0500

    day12

---
+++ day12/project/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+  static void Main()
+  {
+    int x = 10;
+    Foo(x);
+  }
+
+  static void Foo(int a)
+  {
+    int y = a + 5;
+    Bar(y);
+  }
+
+  static void Bar(int b)
+  {
+    int z = b * 2;
+    Console.WriteLine(z);
+  }
+}
