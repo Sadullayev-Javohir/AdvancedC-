e324db350571fee06f899ed75a34f2b08064a9bd
Javohir <javohirsadullayev2024@gmail.com>
2026-02-27 11:37:36 +0500

    day20

---
+++ day20/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿ // string a = "hello";
+ // string b = a;
+ //
+ // a = "world";
+ // Console.WriteLine(b);
+ 
+ 
+
+ for (int i = 0; i < 100000; i++)
+ {
+     string s = "a" + " b";
+     Console.WriteLine("a" + " b");
+ }
