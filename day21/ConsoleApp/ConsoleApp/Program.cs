050e2941205b2d6183bb95b79eca96f6bb435ccf
Javohir <javohirsadullayev2024@gmail.com>
2026-03-03 14:28:08 +0500

    day21

---
+++ day21/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// string a = new string(new[] {'t', 'e', 's', 't'});
+// string b =  "test";
+//
+// Console.WriteLine(object.ReferenceEquals(a, b));
+//
+// a = string.Intern(a);
+// Console.WriteLine(object.ReferenceEquals(a, b));
+
+string a = "test";
+string b = new string(new[] {'t', 'e', 's', 't'});
+
+// Console.WriteLine(string.IsInterned(a) != null);
+Console.WriteLine(string.IsInterned(b));
