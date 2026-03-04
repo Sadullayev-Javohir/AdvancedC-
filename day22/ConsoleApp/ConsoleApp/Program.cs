45e0995536b3d51968f54d52bcf553e08f28adad
Javohir <javohirsadullayev2024@gmail.com>
2026-03-04 17:10:01 +0500

    day2

---
+++ day22/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        CreateObjects();
+        GC.Collect();
+    }
+
+    static void CreateObjects()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Person p = new Person();
+        }
+    }
+}
+
+class Person {}
