64a3e5f2cc6e74bee31248911aa3459d0c7698f6
Javohir <javohirsadullayev2024@gmail.com>
2026-03-05 20:05:17 +0500

    day24

---
+++ day24/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        object obj = new object();
+        
+        Console.WriteLine("Start: " + GC.GetGeneration(obj));
+        
+        GC.Collect(0, GCCollectionMode.Forced, blocking:true, compacting:true);
+        
+        Console.WriteLine("After Gen0 Collect: " + GC.GetGeneration(obj));
+        
+        GC.Collect(1, GCCollectionMode.Forced, blocking:true, compacting:true);
+        Console.WriteLine("After Gen1 Collect: " + GC.GetGeneration(obj));
+        
+        GC.KeepAlive(obj);
+        GC.Collect(2);
+        
+        Console.WriteLine(GC.CollectionCount(0));
+        Console.WriteLine(GC.CollectionCount(1));
+        Console.WriteLine(GC.CollectionCount(2));
+        
+        
+        
+    }
+    
+}
