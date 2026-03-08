ae481ba7dd63ff58fd4f81fe2bd0d211e611f17d
Javohir <javohirsadullayev2024@gmail.com>
2026-03-08 21:38:10 +0500

    day26

---
+++ day26/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Buffers;
+using System.Runtime;
+
+class Program
+{
+    static void Main()
+    {
+        // GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+        // GC.Collect();
+        
+        // ArrayPool<int>
+        byte[] buffer1 = new byte[100000];
+
+        var pool = ArrayPool<byte>.Shared;
+        byte[] buffer = pool.Rent(100000);
+        
+        pool.Return(buffer);
+
+    }
+}
