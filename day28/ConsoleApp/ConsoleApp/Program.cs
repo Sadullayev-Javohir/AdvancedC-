50bdeecaf4db388702f3bb9787aa3be6d71c2d42
Javohir <javohirsadullayev2024@gmail.com>
2026-03-11 10:35:52 +0500

    day28

---
+++ day28/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿class Student
+{
+    public string Name;
+
+    public Student(string name)
+    {
+        Name = name;
+        Console.WriteLine($"{Name} object yaratildi");
+    }
+
+    ~Student()
+    {
+        Console.WriteLine($"{Name} object uchun finalizer ishladi");
+    }
+}
+
+class Program
+{
+    static void CreateStudent()
+    {
+        Student s = new Student("Ali");
+    }
+
+    static void Main()
+    {
+        CreateStudent();
+        Console.WriteLine("CreateStudent tugadi");
+        
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Console.WriteLine("Dastur tugadi");
+    }
+}
