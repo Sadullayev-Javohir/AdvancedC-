015c12f692331683cf59cd243932e0dc0c994f0c
Javohir <javohirsadullayev2024@gmail.com>
2026-03-16 19:38:53 +0500

    day33

---
+++ day33/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿class Person
+{
+    public int Age;
+    public bool IsMarried;
+    public string Name;
+
+    public Person()
+    {
+        Console.WriteLine($"Age: {Age}, IsMarried: {IsMarried}, Name: {Name}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+    }
+}
