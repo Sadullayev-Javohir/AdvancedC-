646db21167679f185636f4a569fbd67e4443659c
Javohir <javohirsadullayev2024@gmail.com>
2026-04-02 21:07:43 +0500

    day41

---
+++ day41/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿class Animal
+{
+    public virtual void Speak() {}
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Dog Barks");
+    }
+}
+
+class Apcherka : Dog
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Apcherka barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal apcherka = new Dog();
+        apcherka.Speak();
+    }
+}
