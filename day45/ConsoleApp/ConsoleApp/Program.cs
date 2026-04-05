75ef87064598dabbb3a1eb97eb0407361c5f1f1b
Javohir <javohirsadullayev2024@gmail.com>
2026-04-05 20:15:43 +0500

    day45

---
+++ day45/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿abstract class Animal
+{
+    // public Animal()
+    // {
+    //     Console.WriteLine("Ctor");
+    // }
+    // public abstract void Speak();
+    public void Sleep() => Console.WriteLine("Sleeping...");
+}
+
+// class Dog : Animal
+// {
+//     public override void Speak()
+//     {
+//         Console.WriteLine("Dog Barks");
+//     }
+// }
+
+class Program
+{
+    static void Main()
+    {
+        // Dog d = new Dog();
+        // d.Speak();
+        // d.Sleep();
+
+        Animal a = new Animal();
+    }
+}
