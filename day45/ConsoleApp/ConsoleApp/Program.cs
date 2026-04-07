ae7f3251f7b7318887ac1ce94fe898d55f619ad2
Javohir <javohirsadullayev2024@gmail.com>
2026-04-07 17:14:48 +0500

    day45

--- day45/ConsoleApp/ConsoleApp/Program.cs
+++ day45/ConsoleApp/ConsoleApp/Program.cs
@@ -1,29 +1,119 @@
-﻿abstract class Animal
-{
-    // public Animal()
-    // {
-    //     Console.WriteLine("Ctor");
-    // }
-    // public abstract void Speak();
-    public void Sleep() => Console.WriteLine("Sleeping...");
-}
-
-// class Dog : Animal
+﻿// // abstract class Animal
+// // {
+// //     // public Animal()
+// //     // {
+// //     //     Console.WriteLine("Ctor");
+// //     // }
+// //     // public abstract void Speak();
+// //     public void Sleep() => Console.WriteLine("Sleeping...");
+// // }
+// //
+// // // class Dog : Animal
+// // // {
+// // //     public override void Speak()
+// // //     {
+// // //         Console.WriteLine("Dog Barks");
+// // //     }
+// // // }
+// //
+// // class Program
+// // {
+// //     static void Main()
+// //     {
+// //         // Dog d = new Dog();
+// //         // d.Speak();
+// //         // d.Sleep();
+// //
+// //         Animal a = new Animal();
+// //     }
+// // }
+// //
+// // interface IPrinter
+// // {
+// //     void Print();
+// // }
+// //
+// // interface IScanner
+// // {
+// //     void Print() {}
+// // }
+// //
+// // class Document : IPrinter
+// // {
+// //     public void Print()
+// //     {
+// //         Console.WriteLine("Printing document...");
+// //     }
+// // }
+// //
+// // class Machine : IPrinter, IScanner
+// // {
+// //     void IPrinter.Print() => Console.WriteLine("Printer printing...");
+// //     void IScanner.Print() => Console.WriteLine("Scanner scanning...");
+// // }   
+// //
+// // class Program
+// // {
+// //     static void Main()
+// //     {
+// //         Machine m = new Machine();
+// //         ((IPrinter)m).Print();
+// //         ((IScanner)m).Print();
+// //     }
+// // }
+//
+// interface ILogger
 // {
-//     public override void Speak()
+//     void Log(string message);
+// }
+//
+// interface IFileLogger
+// {
+//     void Log(string message);
+// }
+//
+// class Logger : ILogger, IFileLogger
+// {
+//     void ILogger.Log(string message) => Console.WriteLine("Console log: " + message);
+//     void IFileLogger.Log(string message) => Console.WriteLine("File log: " + message);
+// }
+//
+// class Program
+// {
+//     static void Main()
 //     {
-//         Console.WriteLine("Dog Barks");
+//         
+//         ILogger consoleLogger = new Logger();
+//         consoleLogger.Log("Hello");
+//
+//         IFileLogger fileLogger = new Logger();
+//         fileLogger.Log("Hello");
 //     }
 // }
+
+interface IPrinter
+{
+    void Print();
+}
+
+interface IScanner
+{
+    void Print();
+}
+
+class Document : IPrinter, IScanner
+{
+    // public void Print() => Console.WriteLine("Printing document");
+    void IPrinter.Print() => Console.WriteLine("Printer printing...");
+    void IScanner.Print() => Console.WriteLine("Scanner printing...");
+}
 
 class Program
 {
     static void Main()
     {
-        // Dog d = new Dog();
-        // d.Speak();
-        // d.Sleep();
-
-        Animal a = new Animal();
+        Document d = new Document();
+        ((IPrinter)d).Print();
+        ((IScanner)d).Print();
     }
 }
--- day45/ConsoleApp/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ day45/ConsoleApp/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646db21167679f185636f4a569fbd67e4443659c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fcc1aafae2f092482c11fd58dd5d86005441f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
