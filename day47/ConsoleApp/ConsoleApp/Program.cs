d8b8d7553a347caeb1363b7331af62079f156b7a
Javohir <javohirsadullayev2024@gmail.com>
2026-04-12 11:36:22 +0500

    day47

---
+++ day47/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿// var item = new OrderItem("Iphone", 16);
+// order.Add(item);
+
+// class Order
+// {
+//     private List<OrderItem> items = new List<OrderItem>();
+//
+//     public void AddItem(string name, int quantity)
+//     {
+//         var item = new OrderItem(name, quantity); // yaratilmoqda
+//         items.Add(item);
+//     }
+// }
+//
+// Player p = new Player("Ali");
+// game.AddPlayer(p);
+
+// class Game
+// {
+//     private List<Player> players = new List<Player>();
+//
+//     public void AddPlayer(string name)
+//     {
+//         var player = new Player(name);
+//         players.Add(player);
+//     }
+// }
+
+
+// class Order
+// {
+//     public List<OrderItem> orders = new List<OrderItem>();
+//
+//     public void AddItem(string name, int quantity)
+//     {
+//         var item = new OrderItem(name, quantity);
+//         orders.Add(item);
+//     }
+// }
+
