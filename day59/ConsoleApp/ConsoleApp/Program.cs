fe0dd576777ad0ff0359ebbd50a5ade171efda57
Javohir <javohirsadullayev2024@gmail.com>
2026-04-27 12:34:14 +0500

    day59

---
+++ day59/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// public interface ICalculateStrategy
+// {
+//     int Execute(int a, int b);
+// }
+//
+// public class AddStrategy : ICalculateStrategy
+// {
+//     public int Execute(int a, int b) => a + b;
+// }
+//
+// public class CalculatorContext
+// {
+//     private ICalculateStrategy _strategy;
+//     public CalculatorContext(ICalculateStrategy strategy) => _strategy = strategy;
+//
+//     public int Run(int a, int b) => _strategy.Execute(a, b);
+// }
+//
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
+
