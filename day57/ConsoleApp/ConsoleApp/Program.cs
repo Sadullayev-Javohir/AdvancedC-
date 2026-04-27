8deeece0688cdcfb84e2b541243a6b9169f5af2b
Javohir <javohirsadullayev2024@gmail.com>
2026-04-27 10:26:21 +0500

    day57

---
+++ day57/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿// LEGAL COMMENTS
+
+// 1. Licence
+// Copyright C (Company)
+
+// 2. Warning
+// Don't change this logic. Payment system depends on it
+
+// 3. TODO
+// TODO: Add validation
+
+// 4. Explanation (murakkab business rule)
+// Discount only applies after 30 days
+
+
+// BAD COMMENTS
+
+// 1. keraksiz comment 
+// increment
+// i++;
+
+// 2. Eski comment
+// returns active users
+// return allUsers;
+
+// 3. Comment bilan yomon nomni yopish
+int d; // days since created
+// daysSinceCreated
+
+
+// O'ZINI TUSHUNTIRUVCHI KOD
+// yomon
+// if (user.Age > 18)
+
+// yaxshi
+// if (user.IsAdult())
+
+// yomon
+// var x = price * 0.9;
+
+// yaxshi
+// var distcountedPrice = price * DistcountRate;
+
+
+// REAL MISOL
+
+// check if user can login
+// if (user.Status == 1 && user.IsVerified == true)
+
+// Self explanatory
+// if (user.CanLogin())
+
+// Inside
+// public bool CanLogin() => IsActive && IsVerified;
+
+
