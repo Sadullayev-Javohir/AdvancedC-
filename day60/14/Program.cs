5f5e29880681af2a68ad5bef8ab30511f1ccbd3f
Javohir <javohirsadullayev2024@gmail.com>
2026-04-28 12:44:43 +0500

    day60

---
+++ day60/14/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+  public static string LongestCommonPrefix(string[] strs)
+  {
+    if (strs == null || strs.Length == 0) return "";
+
+    for (int i = 0; i < strs[0].Length; i++)
+    {
+      char currentChar = strs[0][i];
+      for (int j = 1; j < strs.Length; j++)
+      {
+        if (i >= strs[j].Length || strs[j][i] != currentChar)
+        {
+          return strs[0].Substring(0, i);
+        }
+      }
+    }
+    return strs[0];
+  }
+  static void Main()
+  {
+    var strs = new string[] {"flower", "flight", "flow"};
+    var result = LongestCommonPrefix(strs);
+    Console.WriteLine(result);
+  }
+}
---
+++ day60/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿// YAGNI VIOLATION
+public class PaymentService
+{
+    public void Pay() => Console.WriteLine("Payment completed");
+    public void Refund() => Console.WriteLine("Refund Completed");
+    public void CryptoPayment() => Console.WriteLine("Crypto completed");
+}
+
+
+public class PaymentService2
+{
+    public void Pay() => Console.WriteLine("Payment completed");
+}
+
+// PREMATURE ABSTRACTION
+public interface IMessageSender
+{
+    void Send(string message);
+}
+
+public class EmailSender : IMessageSender
+{
+    public void Send(string message) => Console.WriteLine(message);
+}
+
+// Databaseda YAGNI
+public class User
+{
+    public string Name { get; set; }
+    public string FutureField1 { get; set; }
+    public string FutureField2 { get; set; }
+}
+
+// Configlarda YAGNI
+// {
+//     "Reddis": {},
+//     "RabbitMQ": {},
+//     "Kafka": {}
+// }
+
+// {
+//     "ConnectionString": ""
+// }
+
+// API designda YAGNI
+// [HttpGet]
+// [HttpPost]
+// [HttpPut]
+// [HttpDelete]
+
+
+// Genericlarda YAGNI
+// public class Repository<T>
+// {
+//     
+// }
+
+public class UserRepository
+{}
