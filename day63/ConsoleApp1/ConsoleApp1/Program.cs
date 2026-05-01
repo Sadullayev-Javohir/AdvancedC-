5f67c334c4a8233a00a6394f98027ba02fe2c47f
Javohir <javohirsadullayev2024@gmail.com>
2026-05-01 08:00:09 +0500

    day63

---
+++ day63/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Box<T> where T : notnull
+{
+    public T? Value { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Box<int> e = new();
+        
+        // Box<int?> a = new();
+        // Console.WriteLine(a.Create());
+        // int? x = null;
+        // Nullable<int> x2 = null;
+
+    }
+}
