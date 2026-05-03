c9a0a50ea4bc568cdfe7c8190f3af30b3ab48614
Javohir <javohirsadullayev2024@gmail.com>
2026-05-03 16:13:59 +0500

    day66

---
+++ day66/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Print(int value) => Console.WriteLine(value);
+    static T Echo<T>(T value) => value;
+
+    static void Print<T1, T2>(T1 x, T2 y)
+    {
+        Console.WriteLine(y);
+        Console.WriteLine(x);
+    }
+    static void Main()
+    {
+        Print(4);
+        Print<string>("Hello");
+        Print<string, int>("Somehow", 2);
+    }
+
+    static void Print<T>(T value)
+    {
+        Console.WriteLine(value);
+    }
+}
