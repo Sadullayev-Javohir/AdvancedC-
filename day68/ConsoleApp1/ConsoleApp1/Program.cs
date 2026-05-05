3315a67548115ef799e5dee13660d7835030227c
Javohir <javohirsadullayev2024@gmail.com>
2026-05-05 12:04:36 +0500

    day68

---
+++ day68/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,125 @@
+﻿// class Program
+// {
+//     public delegate void MyDelegate();
+//
+//     static void Hello() => Console.WriteLine("Hello");
+//     static void Main()
+//     {
+//         MyDelegate del = Hello;
+//         del();
+//     }
+// }
+
+// class Test
+// {
+//     public void Show() => Console.WriteLine("Show");
+// }
+//
+// class Program
+// {
+//     public delegate void MyDelegate();
+//     static void Main()
+//     {
+//         Test t = new Test();
+//         MyDelegate del = t.Show;
+//
+//         del.DynamicInvoke();
+//     }
+// }
+//
+// class Program
+// {
+//     public delegate void MyDelegate();
+//
+//     static void A() => Console.WriteLine("A");
+//     static void B() => Console.WriteLine("B");
+//     
+//     static void Main()
+//     {
+//         MyDelegate del = A;
+//         del += B;
+//         
+//         del.Invoke(); 
+//     }
+// }
+
+// class Person
+// {
+//     public void Speak() => Console.WriteLine("Speaking");
+// }
+//
+// class Program
+// {
+//     public delegate void MyDelegate();
+//     static void Main()
+//     {
+//         Person p = new Person();
+//         Action a = p.Speak;
+//         a();
+//     }
+// }
+
+// class Program
+// {
+//     static void Hello() => Console.WriteLine("Hello");
+//
+//     static void Main()
+//     {
+//         Action a = Hello;
+//         a();
+//     }
+// }
+
+// class Program
+// {
+//     static void A() => Console.WriteLine("A");
+//     static void B() => Console.WriteLine("B");
+//     static void C() => Console.WriteLine("C");
+//
+//     static void Main()
+//     {
+//         Action a = A;
+//         a += B;
+//         a += C;
+//
+//         a();
+//     }
+// }
+
+// class Program
+// {
+//     public delegate void MyDelegate();
+//     static void Main()
+//     {
+//         MyDelegate del = Hello;
+//         del();
+//     }
+//
+//     static void Hello() => Console.WriteLine("Hello");
+// }
+
+// class Test
+// {
+//     public void Show() => Console.WriteLine("Show");
+// }
+//
+// class Program
+// {
+//     // public delegate void MyDelegate();
+//     // static void Main()
+//     // {
+//     //     Test t = new Test();
+//     //     MyDelegate del = t.Show;
+//     //     del();
+//     // }
+//     
+//     static void Add() => Console.WriteLine("Add");
+//     public delegate void Operation();
+//
+//     static void Main()
+//     {
+//         Operation op = Add;
+//         op();
+//     }
+// }
+
