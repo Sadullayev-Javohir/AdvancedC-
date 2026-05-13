40064fad6318e41d5d698bf9b212dd45fcd5dde4
Javohir <javohirsadullayev2024@gmail.com>
2026-05-13 09:30:38 +0500

    day74

---
+++ day74/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// class Program
+// {
+//     private static Action store;
+//
+//     static void Main()
+//     {
+//         string bigData = new string('x', 10000);
+//         store = (() => { Console.WriteLine(bigData); });
+//         store = null;
+//         store();
+//     }
+// }
+
