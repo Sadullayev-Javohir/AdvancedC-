cfddcd839e4c3779e9b98e7c45b5d3627f5d525d
Javohir <javohirsadullayev2024@gmail.com>
2026-05-24 16:04:39 +0500

    day85

---
+++ day85/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿[AttributeUsage(AttributeTargets.Class)]
+public class MyInfoAttribute : Attribute
+{
+    public string Author { get; }
+    public MyInfoAttribute(string author) => Author = author;
+}
+
+[MyInfo("Javohir")]
+class MyClass {}
+
+
+// using System.Reflection;
+//
+// [Obsolete("Deprecated")]
+// class Test { }
+//
+// class Program
+// {
+//     static void Main()
+//     {
+//         var type = typeof(Test);
+//
+//         var attributes = type.GetCustomAttributes();
+//
+//         foreach (var attr in attributes)
+//         {
+//             Console.WriteLine(attr);
+//         }
+//     }
+// }
+
+// class Program
+// {
+//     [Obsolete("Bu method eskirgan")]
+//     public static void OldMethod() => Console.WriteLine("Salom");
+//     static void Main()
+//     {
+//         OldMethod();
+//     }
+// }
