94aca07c61ac1bc31775006fb2277803543e0078
Javohir <javohirsadullayev2024@gmail.com>
2026-06-03 11:12:03 +0500

    day95

---
+++ day95/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading;
+
+class Program
+{
+    private static int counter = 0;
+
+    static void Main()
+    {
+        Thread t1 = new Thread(Increment);
+        Thread t2 = new Thread(Increment);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+        
+        Console.WriteLine(counter);
+    }
+
+    static void Increment()
+    {
+        for (int i = 0; i < 100000; i++) Interlocked.Increment(ref counter);
+    }
+}
